ca153f7e2a7c576ed9c82b9185f506596ea605d5
JosephPadfield <josephpadfield@josephpieldsmbp.home>
2024-09-29 10:41:57 +0100

    update

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use DI\Container;
+
+require __dir__ . '/../vendor/autoload.php';
+
+$container = new Container();
+
+$container->set('templating', function () {
+    return new Mustache_Engine([
+        'loader' => new Mustache_Loader_FilesystemLoader(
+            __DIR__ . '/../templates',
+            ['extension' => '']
+        )
+    ]);
+});
+
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args = []) {
+    $html = $this->get('templating')->render('hello.html', [
+        'name' => $args['name']
+    ]);
+    $response->getBody()->write($html);
+    return $response;
+});
+
+
+$app->run();
